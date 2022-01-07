0d7e1e148edb2f85884439a0901d6c48773c0b22
장관우 <gwanwoojang@jang-gwan-uui-MacBookPro.local>
2022-01-07 14:36:04 +0900

    last

--- monthlyproject1/settings.py
+++ monthlyproject1/settings.py
@@ -25,7 +25,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-#(fj$v1jy-u_jckk8diezm-rg)k(v)lrr(b%a@wk6hb7hgoz!0'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ['127.0.0.1', '.herokuapp.com']
 
